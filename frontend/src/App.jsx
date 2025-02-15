5e039bf161b8fa4f254772cd59cd5f9a455209e7
Nijalinga <rajurajappa1984@gmail.com>
2025-02-15 13:59:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/SignUp/signup';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext';
 
