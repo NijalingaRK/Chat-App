937e39455ecbc5cfd13d90c0fcc44e27a7ebb2c0
Nijalinga <rajurajappa1984@gmail.com>
2025-02-15 13:53:23 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const daisyui = require("daisyui");
+import daisyui from "daisyui";
 
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
 };
+
 
 
 
