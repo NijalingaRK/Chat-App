3f8f95d1211368ef4eb8fa407631471ac8df9f1a
Niju <nijalingar@gmail.com>
2025-02-15 14:22:51 +0530

    Updated MongoDB connection string

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,12 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	// eslint-disable-next-line no-undef
+// 	plugins: [require("daisyui")],
+// };
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 
